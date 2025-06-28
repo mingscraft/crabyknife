6c8dc79d744286fe8fa3a48da261a6ff14eec663
mingscraft <ray.vhatt@gmail.com>
2025-06-28 01:09:11 +0100

    [feat] add ping subcommand (#5)
    * add ping subcommand
    
    * update version and changelog
    
    * remove doc tests that does not work

--- src/commandline.rs
+++ src/commandline.rs
@@ -1,8 +1,9 @@
-use crate::prettify_xml;
+use crate::{ping, prettify_xml};
 
 pub enum Subcommands {
     PrettifyXml,
     NewUuid,
+    Ping,
 }
 
 impl std::str::FromStr for Subcommands {
@@ -12,6 +13,7 @@ impl std::str::FromStr for Subcommands {
         match s {
             "prettify-xml" => Ok(Self::PrettifyXml),
             "new-uuid" => Ok(Self::NewUuid),
+            "ping" => Ok(Self::Ping),
             _ => Err("support subcommands"),
         }
     }
@@ -26,6 +28,7 @@ pub fn run(
     match subcommand {
         Subcommands::PrettifyXml => handle_prettify_xml(remaining_args),
         Subcommands::NewUuid => handle_new_uuid(),
+        Subcommands::Ping => handle_ping(remaining_args),
     }
 }
 
@@ -34,7 +37,7 @@ fn handle_prettify_xml(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let xml = remaining_args
         .next()
-        .expect("usage: crabyknif prettify-xml <unprettified xml>");
+        .expect("Usage: crabyknif prettify-xml <unprettified xml>");
 
     let prettified = prettify_xml::prettify_xml(&xml)?;
     println!("{prettified}");
@@ -43,5 +46,14 @@ fn handle_prettify_xml(
 
 fn handle_new_uuid() -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", uuid::Uuid::new_v4());
+    Ok(())
+}
+
+fn handle_ping(mut remaining_args: std::env::Args) -> Result<(), Box<dyn std::error::Error>> {
+    let target = remaining_args
+        .next()
+        .expect("Usage: crabbyknife ping <host>");
+
+    ping::ping(&target)?;
     Ok(())
 }
